7408e87dfddfe1811d400f39d2febe6c5a57ab67
Rahul Salunkhe <81208182+rsrahulsalunkhe@users.noreply.github.com>
2023-07-27 11:18:05 +0530

    Update Find-SInductive.py

--- Find-SInductive.py
+++ Find-SInductive.py
@@ -1,22 +1,31 @@
 #Implement the Find-S Inductive Learning algorithm.
-import csv
-a = []
-with open('enjoysport.csv', 'r') as csvfile:
- for row in csv.reader(csvfile):
-   a.append(row)
-   print(a)
-print("\n The total number of training instances are : ",len(a))
-# num_attribute = len(a[0])-1
-# print("\n The initial hypothesis is : ")
-# hypothesis = ['0']*num_attribute
-# print(hypothesis)
-# for i in range(0, len(a)):
-#  if a[i][num_attribute] == 'yes':
-#   for j in range(0, num_attribute):
-#     if hypothesis[j] == '0' or hypothesis[j] == a[i][j]:
-#        hypothesis[j] = a[i][j]
-#     else:
-#        hypothesis[j] = '?'
-# print("\n The hypothesis for the training instance {} is : \n" .format(i+1),hypothesis)
-# print("\n The Maximally specific hypothesis for the training instance is ")
-# print(hypothesis)
+
+def initilize_hypothesis(attributs):
+    return ["0"] * len(attributs)
+
+def find_s_algo(training_data):
+    attributs = trainig_data[0][:-1]
+    hypothesis = initilize_hypothesis(attributs)
+    
+    for i in trainig_data:
+        x, y = i[:-1], i[-1]
+
+        if y == "Y":
+            for j in range(len(attributs)):
+                if hypothesis[j] == "0":
+                    hypothesis[j] = x[j]
+                elif hypothesis[j] != x[j]:
+                    hypothesis[j] = "?"
+
+    return hypothesis
+        
+
+trainig_data = [
+    ["Sunny", "Warm", "Normal", "Strong", "Warm", "Same", "Y"],
+    ["Sunny", "Warm", "High", "Strong", "Warm", "Same", "Y"],
+    ["Rainy", "Cold", "High", "Strong", "Warm", "Change", "N"],
+    ["Sunny", "Warm", "Normal", "Strong", "Cood", "Change", "Y"]
+]
+
+hypothesis = find_s_algo(trainig_data)
+print("Final Hypothesis", hypothesis)
